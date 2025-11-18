880a19c6b363ffd81ddcbeb0bce0acd71fffdf71
José Leonardo Lemos <covero.fbs@gmail.com>
2025-11-18 21:12:46 +0000

    ainda não deu certo a publicação no pages do github

---
+++ src/next.config.ts
@@ -0,0 +1,38 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  // Required for Next.js to run in a non-root path
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/ferramentas' : '',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
