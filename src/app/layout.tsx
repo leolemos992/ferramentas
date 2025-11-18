364faba6cbb35b9f8b30e44bed27d3e0599a3f20
José Leonardo Lemos <covero.fbs@gmail.com>
2025-11-18 14:49:46 +0000

    incremente as notas de versão

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-  const version = "0.0.0.4"; 
+  const version = "0.0.0.5"; 
 
   return (
     <footer className="w-full bg-background border-t border-border mt-auto">
