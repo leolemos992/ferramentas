9c4ce27d3d4b2090dec2f0272fda06daa900f882
José Leonardo Lemos <covero.fbs@gmail.com>
2025-11-18 18:20:36 +0000

    ficou dessa forma, está certo?

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type {NextConfig} from 'next';
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: isProd ? '/ferramentas' : '',
+  assetPrefix: isProd ? '/ferramentas/' : '',
   output: 'export',
   typescript: {
     ignoreBuildErrors: true,
